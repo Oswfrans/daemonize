d4b33269b6b91298c594e49cf7331b6933418fc7
Ramin Etemaadi <ramin@payvision.com>
2018-10-12 09:36:31 +0200

    fix test

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -2,11 +2,14 @@ package controllers
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import ml.combust.bundle.BundleFile
+import ml.combust.mleap.runtime.MleapSupport._
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.libs.json.Json
 import play.api.test._
 import play.api.test.Helpers._
+import resource._
 
 /**
  * Add your spec here.
@@ -19,6 +22,13 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
   Helpers.stubControllerComponents(
     playBodyParsers = Helpers.stubPlayBodyParsers(materializer)
   )
+
+  private val mlModelPath = this.getClass.getClassLoader.getResource("ml-model.zip").getPath
+  private val zipBundleM = (for(bundle <- managed(BundleFile(s"jar:file:$mlModelPath"))) yield {
+    bundle.loadMleapBundle().get
+  }).opt.get
+  private val mleapPipeline = zipBundleM.root
+
   private val testJson = Json.obj(
     "firstsixdigits" -> "123456",
     "cvvresponse" -> "123",
@@ -27,10 +37,11 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
   )
   @transient private val request = FakeRequest(POST, "/").withBody(testJson)
 
+
   "HomeController POST" should {
 
     "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
+      val controller = new HomeController(stubControllerComponents(), mleapPipeline)
       val home = controller.index().apply(request)
 
       status(home) mustBe OK
